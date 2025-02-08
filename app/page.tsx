4888028ca27f397cbfdcc4692a6ad22e7baad10c
Ria Arora <riaarora@Rias-MacBook-Air.local>
2025-02-08 21:28:18 +0530

    home page corrected

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,92 @@
-import Image from "next/image";
+'use client'
+import { getMyTime } from '@/actions/auth';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+function Countdown({ targetDate }: { targetDate: number }) {
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(targetDate));
+    const router = useRouter()
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(calculateTimeLeft(targetDate));
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, [targetDate]);
+
+    function calculateTimeLeft(targetDate: number) {
+        const difference = targetDate - Date.now();
+        let timeLeft = {
+            years: 0,
+            months: 0,
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+        };
+
+        if (difference > 0) {
+            timeLeft = {
+                years: Math.floor(difference / (1000 * 60 * 60 * 24 * 365)),
+                months: Math.floor((difference % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30)),
+                days: Math.floor((difference % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+                minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+                seconds: Math.floor((difference % (1000 * 60)) / 1000),
+            };
+        }
+
+        return timeLeft;
+    }
+
+    function logout(){
+      localStorage.clear()
+      router.replace("/login")
+    }
+
+    return (
+        <div className='h-screen w-screen relative' style={{backgroundImage: "url('/ana.jpg')", backgroundSize:"cover", backgroundPosition:"center"}}>
+            <div className='h-[100vh] w-1/2'>
+                <div className='h-15vh flex justify-center items-center p-10'>
+                    <h1 className='text-6xl text-white font-bold'>Your Clock is ticking !</h1>
+                </div>
+                <div className='w-full flex justify-center items-center flex-col pr-16 ml-10'>
+                    <div className='text-6xl font-bold text-center flex flex-col gap-4'>
+                        <div className='bg-white/20 backdrop-blur-md px-4 py-2'>{timeLeft.years} Years </div>
+                        <div className='bg-white/20 backdrop-blur-md px-4 py-2'>{timeLeft.months} Months </div>
+                        <div className='bg-white/20 backdrop-blur-md px-4 py-2'>{timeLeft.days} Days </div>
+                        <div className='bg-white/20 backdrop-blur-md px-4 py-2'>{timeLeft.hours} Hours </div>
+                        <div className='bg-white/20 backdrop-blur-md px-4 py-2'>{timeLeft.minutes} Minutes </div>
+                        <div className='bg-white/20 backdrop-blur-md px-4 py-2'>{timeLeft.seconds} Seconds</div>
+                    </div>
+                </div>
+            </div>
+            <button className='absolute top-5 right-5' onClick={logout}>LOG OUT</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
+}
+
+export default function Page() {
+    const [timestamp, setTimestamp] = useState<number | null>(null);
+    const router = useRouter()
+    useEffect(() => {
+        async function fetchTimestamp() {
+            const email = localStorage.getItem("email")
+            if(!email){
+              router.replace("/login")
+              return
+            }
+            const ts = await getMyTime(email);
+            setTimestamp(new Date(ts).getTime());
+        }
+
+        fetchTimestamp();
+    }, []);
+
+    if (timestamp === null) {
+        return <div className='text-white'>Loading...</div>;
+    }
+
+    return <Countdown targetDate={timestamp} />;
 }
